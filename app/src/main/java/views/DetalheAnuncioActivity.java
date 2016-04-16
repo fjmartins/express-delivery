d758065b1f160e8b28c076501392b5b76808ef84
KleberDarlan <kleberdarlanmonte@gmail.com>
2016-04-16 16:37:19 -0300

    Criando estrutura inicial da tela de Detalhe de Anúncio

---
+++ app/src/main/java/views/DetalheAnuncioActivity.java
@@ -0,0 +1,15 @@
+package views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.anderson.expressdelivery.R;
+
+public class DetalheAnuncioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detalhe_anuncio_activity);
+    }
+}
