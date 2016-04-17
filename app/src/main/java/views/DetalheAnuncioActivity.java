b3f54e8af401effe831ef97898ca660b4a5223e5
KleberDarlan <kleberdarlanmonte@gmail.com>
2016-04-17 02:41:43 -0300

    Criando médoto para carregar dados vindos da tela princial

--- app/src/main/java/views/DetalheAnuncioActivity.java
+++ app/src/main/java/views/DetalheAnuncioActivity.java
@@ -8,6 +8,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -20,37 +21,48 @@ import com.example.anderson.expressdelivery.R;
 
 public class DetalheAnuncioActivity extends AppCompatActivity {
 
-    private String titulo, descricao, telefone, endereco;
-    private Bitmap foto;
+    private TextView titulo, descricao, telefone, endereco;
+    private ImageView foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detalhe_anuncio_activity);
 
-        this.titulo = ((TextView)findViewById(R.id.txtDetAnuncTitulo)).getText().toString();
-        this.descricao = ((TextView)findViewById(R.id.txtDetAnuncDesc)).getText().toString();
-        this.telefone = ((TextView)findViewById(R.id.txtDetAnuncTelefone)).getText().toString();
-        this.endereco = ((TextView)findViewById(R.id.txtDetAnuncEndereco)).getText().toString();
-        this.foto = (findViewById(R.id.imgDetAnuncImagem)).getDrawingCache();
+        this.titulo = (TextView)findViewById(R.id.txtDetAnuncTitulo);
+        this.descricao = (TextView)findViewById(R.id.txtDetAnuncDesc);
+        this.telefone = (TextView)findViewById(R.id.txtDetAnuncTelefone);
+        this.endereco = (TextView)findViewById(R.id.txtDetAnuncEndereco);
+        this.foto = (ImageView)findViewById(R.id.imgDetAnuncImagem);
+
+        carregaDados();
     }
 
     public void atualizar(View v) {
 //        Intent intent = new Intent(this, CadastroAnuncioActivity.class);
-//        intent.putExtra("titulo", this.titulo);
-//        intent.putExtra("descricao", this.descricao);
-//        intent.putExtra("telefone", this.telefone);
-//        intent.putExtra("endereco", this.endereco);
-//        intent.putExtra("fotoBitmap", this.foto);
+//        intent.putExtra("titulo", this.titulo.getText().toString());
+//        intent.putExtra("descricao", this.descricao.getText().toString());
+//        intent.putExtra("telefone", this.telefone.getText().toString());
+//        intent.putExtra("endereco", this.endereco.getText().toString());
+//        intent.putExtra("fotoBitmap", this.foto.getDrawingCache());
 //        startActivity(intent);
     }
 
     public void goBack(View v) {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, PrincipalActivity.class);
         startActivity(intent);
     }
 
     public void carregaDados() {
-        
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null) {
+            this.titulo.setText(extras.getString("titulo"));
+            this.descricao.setText(extras.getString("descricao"));
+            this.telefone.setText(extras.getString("telefone"));
+            this.endereco.setText(extras.getString("endereco"));
+            Bitmap bitmap = (Bitmap)extras.getParcelable("fotoBitmap");
+            this.foto.setImageBitmap(bitmap);
+        }
     }
 }
