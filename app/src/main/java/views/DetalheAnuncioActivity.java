2a8adf58324265b5109352a7c13433f6cb27c41b
KleberDarlan <kleberdarlanmonte@gmail.com>
2016-04-17 02:14:12 -0300

    Desacoplando conteúdo do layout da tela de detalhe de anúncios

--- app/src/main/java/views/DetalheAnuncioActivity.java
+++ app/src/main/java/views/DetalheAnuncioActivity.java
@@ -1,19 +1,27 @@
 package views;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.example.anderson.expressdelivery.R;
 
-public class DetalheAnuncioActivity extends Activity {
+public class DetalheAnuncioActivity extends AppCompatActivity {
 
     private String titulo, descricao, telefone, endereco;
-    private Image foto;
+    private Bitmap foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class DetalheAnuncioActivity extends Activity {
         this.descricao = ((TextView)findViewById(R.id.txtDetAnuncDesc)).getText().toString();
         this.telefone = ((TextView)findViewById(R.id.txtDetAnuncTelefone)).getText().toString();
         this.endereco = ((TextView)findViewById(R.id.txtDetAnuncEndereco)).getText().toString();
+        this.foto = (findViewById(R.id.imgDetAnuncImagem)).getDrawingCache();
     }
 
     public void atualizar(View v) {
@@ -32,6 +41,16 @@ public class DetalheAnuncioActivity extends Activity {
 //        intent.putExtra("descricao", this.descricao);
 //        intent.putExtra("telefone", this.telefone);
 //        intent.putExtra("endereco", this.endereco);
+//        intent.putExtra("fotoBitmap", this.foto);
 //        startActivity(intent);
+    }
+
+    public void goBack(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void carregaDados() {
+        
     }
 }
