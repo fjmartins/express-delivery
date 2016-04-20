39a20cadf7f7b88bd95b2bbd98ee4ee24b205a6d
Anderson Nascimento <and.pax@gmail.com>
2016-04-20 00:35:04 -0300

    Ajuste na tela de cadastro

--- app/src/main/java/views/CadastroActivity.java
+++ app/src/main/java/views/CadastroActivity.java
@@ -1,15 +1,27 @@
 package views;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.anderson.expressdelivery.R;
 
-public class CadastroActivity extends AppCompatActivity {
+public class CadastroActivity extends Activity {
+
+    private Button btCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_activity);
+
+        this.btCadastrar = (Button) findViewById(R.id.btnCadCadastrar);
+    }
+
+    public void logar(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
