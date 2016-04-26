34f052a787b5d7f1d0019bc9da5a76f7f2f5d520
KleberDarlan <kleberdarlanmonte@gmail.com>
2016-04-26 01:39:18 -0300

    Criando controlador para modelo Announcement

---
+++ app/src/main/java/controllers/AnnouncementController.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import java.util.List;
+
+import dao.IAnnouncementDao;
+import models.Announcement;
+import services.IResult;
+import webservices.AnnouncementParse;
+
+/**
+ * Created by kleber on 26/04/16.
+ */
+public class AnnouncementController {
+
+    private static IAnnouncementDao announcementDao = new AnnouncementParse();
+
+    public static void insert(final Announcement announcement, final IResult<Announcement> result) {
+        announcementDao.insert(announcement, new IResult<Announcement>() {
+            @Override
+            public void onSuccess(List<Announcement> list) {
+
+            }
+
+            @Override
+            public void onSuccess(Announcement obj) {
+                result.onSuccess(obj);
+            }
+
+            @Override
+            public void onError(String msg) {
+                result.onError(msg);
+            }
+        });
+    }
+
+    public static void getAll(final IResult<Announcement> result) {
+        announcementDao.getAll(new IResult<Announcement>() {
+            @Override
+            public void onSuccess(List<Announcement> list) {
+                result.onSuccess(list);
+            }
+
+            @Override
+            public void onSuccess(Announcement obj) {
+
+            }
+
+            @Override
+            public void onError(String msg) {
+                result.onError(msg);
+            }
+        });
+    }
+
+}
