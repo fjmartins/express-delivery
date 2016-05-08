ceee9f42de4e40bf9460401f57377a63e6d79c09
AllanFelicio <allan_felicio@hotmail.com>
2016-05-08 12:27:16 -0300

    Criação da tela do envio de proposta, alteração da logo, e inclusão de um fundo neutro branco com uma textura (sugerido por kleber). Além de ajustes na layouts, criação da class de enviar proposta (contendo apenas a referência das variáveis para os campos), incluisão da activity no manifest, e alteração da logo e da barra da tela de login.

---
+++ app/src/main/java/views/SubmitProposalActivity.java
@@ -0,0 +1,32 @@
+package views;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.anderson.expressdelivery.R;
+
+/**
+ * Created by Allan-PC on 08/05/2016.
+ */
+public class SubmitProposalActivity extends GenericActivity {
+
+    private EditText title, description, valor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.submit_proposal_activity);
+
+        this.isUserAuth(this);
+
+        this.title = (EditText) findViewById(R.id.txtEnvioPropostaTitulo);
+        this.description = (EditText) findViewById(R.id.txtEnvioPropostaDesc);
+        this.valor = (EditText) findViewById(R.id.txtEnvioPropostaValor);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.isUserAuth(this);
+    }
+}
